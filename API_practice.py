4282c60beb0d1db75d3ad0c17d7c6fe8a59a944c
ominkina <ominkina@gmail.com>
2018-08-30 17:11:39 -0400

    NYTs API tutorial

--- API_practice.py
+++ API_practice.py
@@ -1,11 +1,18 @@
-#Python API tutorial
+#Python API tutorials (Installed requests package: $sudo pip install requests)
+#Tutorial used: https://www.dataquest.io/blog/python-api-tutorial/
 
+#1. ISS location
 #import requests library
 import requests 
 #query for ISS location
 response = requests.get("http://api.open-notify.org/iss-now.json")
 #get status
 print response
-<Response [200]>
 #pring content of response
 print(response.content)
+
+#2. NYT (Installed nytimesarticle package, which access to API through python; $sudo pip install nytimesarticle)
+#Tutorial used: http://dlab.berkeley.edu/blog/scraping-new-york-times-articles-python-tutorial
+from nytimesarticle import articleAPI
+api = articleAPI('4e605e42050a4dcab09a95d78d36b7dd')
+
