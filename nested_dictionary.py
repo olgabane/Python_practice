060803248f1bbf3db3a356f39f05b6f471087b8e
ominkina <ominkina@gmail.com>
2018-09-07 14:14:01 -0400

    Nested dictionary practice

---
+++ nested_dictionary.py
@@ -0,0 +1,8 @@
+#Nested dictionary practice
+
+people = {1: {'Name': 'John', 'Age': '27', 'Sex': 'Male'},
+          2: {'Name': 'Marie', 'Age': '22', 'Sex': 'Female'}}
+
+for k, v in people.items():
+    print(k, \n)
+    print(v, \n)
